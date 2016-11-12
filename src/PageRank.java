f71bd1d1a37db392b92096580c660053ce95c0dd
Denis <vite0150@gmail.com>
2016-11-12 00:17:40 -0600

    Actualizado: main para pruebas archivos

--- src/PageRank.java
+++ src/PageRank.java
@@ -1,9 +1,20 @@
 
+import Model.Archivos;
+import java.io.IOException;
+
+
 public class PageRank {
 
-    public static void main(String[] args) {
-        
-        System.out.println("Hola mundo");
+    public static void main(String[] args) throws IOException {
+        Archivos af=new Archivos();
+        af.cargar();
+        for (int i = 0; i < 11; i++) {
+            System.out.print("|");
+            for (int j = 0; j < 11; j++) {
+                System.out.print(af.matriz[i][j]);
+            }
+            System.out.println("|");
+        }
     }
     
 }
